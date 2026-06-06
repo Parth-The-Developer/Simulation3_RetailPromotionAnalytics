3e529dd37e0840ae80064762c49ab58bd166e031
Parth Patel <parthmpatel122002@gmail.com>
2026-06-06 12:48:18 -0400

    revert create_schema.sql to team version (keep shared file unchanged)

--- scripts/schema/create_schema.sql
+++ scripts/schema/create_schema.sql
@@ -1,18 +1,32 @@
+ 
+ 
 -- 1) Project database
+ 
 IF DB_ID(N'RetailPromotionAnalytics') IS NULL
+BEGIN
     CREATE DATABASE RetailPromotionAnalytics;
+    PRINT 'Database [RetailPromotionAnalytics] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Database [RetailPromotionAnalytics] already exists - skipped.';
+END
 GO
 
 USE RetailPromotionAnalytics;
 GO
-PRINT 'Database [RetailPromotionAnalytics] created.';
-GO
-
+ 
 -- 2) RetailAnalytics schema
+ 
 IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'RetailAnalytics')
+BEGIN
     EXEC ('CREATE SCHEMA RetailAnalytics AUTHORIZATION dbo;');
-GO
-PRINT 'Schema [RetailAnalytics] created.';
+    PRINT 'Schema [RetailAnalytics] created.';
+END
+ELSE
+BEGIN
+    PRINT 'Schema [RetailAnalytics] already exists - skipped.';
+END
 GO
 
 PRINT 'Foundation ready: database RetailPromotionAnalytics + schema RetailAnalytics. You can start your work.';
